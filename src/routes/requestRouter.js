4bc24bb0e0eb1a0a9ac897eb098ca5602549c181
Avipriya <avipriya.pal2000@gmail.com>
2024-12-08 18:42:08 +0530

    Request review api has been built successfully

--- src/routes/requestRouter.js
+++ src/routes/requestRouter.js
@@ -41,4 +41,37 @@ requestRouter.post('/request/send/:status/:toUserId', authUserMiddleware, async 
     }
 });
 
+requestRouter.post('/request/review/:status/:requestId', authUserMiddleware,async(req,res)=>{
+    try{
+        const status = req.params.status;
+        const requestId = req.params.requestId;
+
+        const user = req.user;
+
+        const acceptedStatus = ["accepted","rejected"];
+
+        if(!acceptedStatus.includes(status)){
+            return res.status(400).send("Status is invalid!");
+        }
+        const findDetailConnectionRequest = await connectionRequest.findOne({
+            _id: requestId,
+            toUserId: user._id,
+            status: "interested"
+        });
+        console.log(findDetailConnectionRequest);
+        if(!findDetailConnectionRequest){
+            return res.status(400).send("Connection request does not exist");
+        }
+        findDetailConnectionRequest.status = status;
+
+        const data = await findDetailConnectionRequest.save();
+
+        res.status(200).json({"message":`Request has been ${status}`,data});
+
+    }
+    catch(err){
+        res.status(400).send("Something Went Wrong!" + err.message);
+    }
+})
+
 module.exports = { requestRouter };
